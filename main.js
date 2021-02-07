0e67d11d096275a571c7d1815b90794241f88546
author <jffyw@hotmail.com>
2021-02-08 03:46:16 +1100

    navbar debugging

--- main.js
+++ main.js
@@ -1,58 +1,50 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
-var header = document.getElementById("header");
+const navbar = document.getElementById("navbar");
+const header = document.querySelector('.logo-text');
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// Get the offset position and height of the navbar at load and resize
+let sticky = navbar.offsetTop;
+let navHeight = navbar.offsetHeight;
+window.onresize = () => {
+  sticky = window.innerHeight/5;
+  navHeight = navbar.offsetHeight;
+}
 
+window.onkeydown = (e) => {
+  if (e.keyCode == 82) {
+    console.log(`scroll: ${window.pageYOffset}\nnavHeight: ${navHeight}\nnavOffset: ${sticky}\nheaderHeight: ${header.offsetHeight}`);
+  } else if (e.keyCode == 81) {
+    // navbar.style.position = 'fixed';
+    // navbar.style.width = '100%';
+    navbar.classList.add('sticky');
+    // let str = `calc(${navHeight}px + 20vh)`;
+    // ^^^^ problem statement
+    header.style.height = '100px';
+  } else if (e.keyCode == 84) {
+    // navbar.style.position = '';
+    // navbar.style.width = '';
+    navbar.classList.remove('sticky');
+    header.style.height = '20vh';
+  }
+
+}
+
+console.log(sticky);
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
-    header.classList.add("header-sticky")
+    // header.classList.add("header-sticky")
+    let str = `calc(${navHeight}px + 20vh)`;
+    console.log(str);
+    header.style.height = str;
   } else {
+    console.log("here");
     navbar.classList.remove("sticky");
-    header.classList.remove("header-sticky")
+    // header.classList.remove("header-sticky")
+    header.style.height = '20vh';
   }
 }
-
-//Makes input boxes adaptive to length of input
-// $(document).ready(function () {
-//     var $inputs = $('.resizing-input');
-
-//     // Resize based on text if text.length > 0
-//     // Otherwise resize based on the placeholder
-//     function resizeForText(text) {
-//         var $this = $(this);
-//         if (!text.trim()) {
-//             text = $this.attr('placeholder').trim();
-//         }
-//         var $span = $this.parent().find('span');
-//         $span.text(text);
-//         var $inputSize = $span.width();
-//         $this.css("width", $inputSize);
-//     }
-
-//     $inputs.find('input').keypress(function (e) {
-//         if (e.which && e.charCode) {
-//             var c = String.fromCharCode(e.keyCode | e.charCode);
-//             var $this = $(this);
-//             resizeForText.call($this, $this.val() + c);
-//         }
-//     });
-
-//     // Backspace event only fires for keyup
-//     $inputs.find('input').keyup(function (e) { 
-//         if (e.keyCode === 8 || e.keyCode === 46) {
-//             resizeForText.call($(this), $(this).val());
-//         }
-//     });
-
-//     $inputs.find('input').each(function () {
-//         var $this = $(this);
-//         resizeForText.call($this, $this.val())
-//     });
-// });
